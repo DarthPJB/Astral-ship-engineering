9ed51f17c0a79142895b83561270c24851e36b38
John Bargman <darthpjb@gmail.com>
2021-04-14 01:25:10 +0100

    started placement of screw_posts

--- project_dir/mikebike/88_assembly/casing.py
+++ project_dir/mikebike/88_assembly/casing.py
@@ -7,6 +7,12 @@ box_corner_fillet = 15;
 box_wall_thickness = 5;
 box_total_width = 85;
 
+screw_thread_diam = 5;
+screw_post_diam = screw_thread_diam + 3;
+screw_depth = 40;
+screw_cap_diam = 9;
+screw_cap_height = 4;
+
 
 ## ------------- Setup Variables
 
@@ -24,11 +30,11 @@ points_list.append([150.52, points_list[1][1] + 127.04 ]);
 
 # ---------- Generate Core Geometary
 
-result = cq.Workplane("XY");
+perimeter = cq.Workplane("XY");
 # Generate plane containing wire-loop
 for point in points_list:
-    result = result.polarLine(point[0],point[1]);
-perimeter = result.close()
+    perimeter = perimeter.polarLine(point[0],point[1]);
+perimeter = perimeter.close()
 
 # shrink the wire-loop to handle tolerance and wall box_wall_thickness
 casing_edge = perimeter.edges().offset2D(-(fit_tolerance_bike + box_wall_thickness))
@@ -39,10 +45,19 @@ casing_geometry = casing_geometry.edges("|Z").fillet(box_corner_fillet);
 # Shell the casing to make the inside hollow.
 casing_geometry = casing_geometry.shell(box_wall_thickness,"arc");
 
-#casing_top = casing_geometry.faces(">Z").workplane(-box_wall_thickness).split(keepTop=True)
+casing_top = casing_geometry.faces(">Z").workplane(-box_wall_thickness).split(keepTop=True)
 casing_bottom = casing_geometry.faces(">Z").workplane(-box_wall_thickness).split(keepTop=False,keepBottom=True);
 
+screw_placements = casing_top.faces(">Z").wires().toPending().offset2D(-screw_post_diam/2).vertices()
+
+screw_posts  = cq.Workplane("XY");
+
+screw_placements = screw_placements#.extrude(-box_total_width)
+
+for x in range(8):
+    if(x != 6):
+        screw_posts = screw_posts.add(screw_placements.item(x).vertices().workplane().circle(screw_post_diam).extrude(-80))
+#screw_posts = screw_posts.wire()#.extrude(box_total_width)
 
 
-
-show_object(casing_bottom)
+show_object(screw_posts)
