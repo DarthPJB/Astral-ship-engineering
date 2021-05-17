06de5442c8d84dc01d7bf8671dfe70aab45e6b20
John Bargman <darthpjb@gmail.com>
2021-05-17 18:17:23 +0100

    Final changes completed; box now meets criteria.

--- project_dir/mikebike/88_assembly/casing.py
+++ project_dir/mikebike/88_assembly/casing.py
@@ -23,9 +23,30 @@ box_wall_thickness = 5; # thickness of casing walls
 
 ##Lid creation settings
 lid_casing_split_distance = 10;
-lid_casing_split_offset = 30;
-Lid_Lapping_Offset = box_wall_thickness
+lid_casing_split_offset = 60;
+lid_Lapping_Offset = box_wall_thickness
 lid_lapping_Thickness = (box_wall_thickness+fit_tolerance_doubled)/2
+
+##Screw-placement and settings
+screw_placement_distance = 60;
+screw_thread_diam = 5 - fit_tolerance_casing; # Screw Thread diameter for connecting face-plate to casing
+screw_post_diam = screw_thread_diam + box_wall_thickness; # screw-post-width pre-calulation
+screw_depth = 40 + box_wall_thickness;   # length of screw-fitting into the box itself
+screw_cap_diam = 9 + fit_tolerance_doubled; # Size of screw-cap for recessed fitting
+screw_cap_height = 2 + fit_tolerance_casing; # Number of milimemeters to recess the screw-caps
+# define per-step differences for screw-hole placement                              --- TODO: generate these according to generative geometary
+screw_rising_offset = [55, 26.5];
+screw_rising_difference = [-8.5,-1]
+screw_falling_offset = [8,screw_placement_distance];
+
+# Text to be placed on the box - note custom font use, should be in CQ's working path
+cut_text_position_x = 180;
+cut_text_position_y = 60;
+cut_text_angle = 24;
+cut_text = "Created By @Astral_3D";
+cut_text_font = "Trueno Bold";
+cut_text_fontPath = "truenobd.otf"
+
 
 ## Point lists for generating geometary                                         ---                     Point Lists
 
@@ -58,37 +79,17 @@ if DEBUG_MODE: log(Maximum_Size);
 Xpos = (Maximum_Size[0] / 2) + lid_casing_split_offset;
 Ypos = (Maximum_Size[1] / 4)
 Bezier_Cut_Points = [
-    (Xpos, Ypos*1),
-    (Xpos-lid_casing_split_offset, Ypos*2),
-    (Xpos, Ypos*3)
+    (Xpos- lid_casing_split_offset, Ypos*1),
+    (Xpos, Ypos*2),
+    (Xpos- lid_casing_split_offset, Ypos*3)
 ]
-
-
-##Screw-placement and settings
-screw_placement_distance = 60;
-screw_thread_diam = 5; # Screw Thread diameter for connecting face-plate to casing
-screw_post_diam = screw_thread_diam + box_wall_thickness; # screw-post-width pre-calulation
-screw_depth = 40;   # length of screw-fitting into the box itself
-screw_cap_diam = 9.1; # Size of screw-cap for recessed fitting
-screw_cap_height = 2; # Number of milimemeters to recess the screw-caps
-# define per-step differences for screw-hole placement                              --- TODO: generate these according to generative geometary
-screw_rising_offset = [55, 26.5];
-screw_rising_difference = [-8.5,-1]
-screw_falling_offset = [8,screw_placement_distance];
 
 ##Cable Hole Settings
 Cable_hole_diam = 20; # Size of the cable-hole (20mm is a good fit for cable-plugs)
-Cable_hole_position = cq.Vector((box_corner_fillet * 2) + box_wall_thickness, box_total_width/2, Maximum_Size[0]);
+Cable_hole_position = cq.Vector((box_corner_fillet * 3) + box_wall_thickness, box_total_width/2, Maximum_Size[0]);
 Cable_hole_angle = 0;
-Cable_hole_Cut_Depth = 200;
+Cable_hole_Cut_Depth = Maximum_Size[1]/4;
 
-# Text to be placed on the box - note custom font use, should be in CQ's working path
-cut_text_position_x = 180;
-cut_text_position_y = 60;
-cut_text_angle = 24;
-cut_text = "Created By @Astral_3D";
-cut_text_font = "Trueno Bold";
-cut_text_fontPath = "truenobd.otf"
 
 
 
@@ -134,11 +135,11 @@ text_cut_volume = text.union(text2);
 if DEBUG_MODE: debug(text_cut_volume, name='text-cutting volume');
 
 # Generate the Spline-bezier cut and repepeat to creat lapping pannels              ---                     Creating lapping pannel Inverse-Geometary
-Cut_Volume = cq.Workplane("XY").workplane(-lid_lapping_Thickness*2).center(Lid_Lapping_Offset,0)\
+Cut_Volume = cq.Workplane("XY").workplane(-lid_lapping_Thickness*2).center(lid_Lapping_Offset,0)\
 .lineTo(Maximum_Size[0]/2, 0).spline(Bezier_Cut_Points,includeCurrent=True).lineTo(0,Maximum_Size[1]/2).close()\
 .extrude(lid_lapping_Thickness);
 
-Cut_Volume2 = cq.Workplane("XY").workplane(lid_lapping_Thickness + box_total_width).center(Lid_Lapping_Offset,0)\
+Cut_Volume2 = cq.Workplane("XY").workplane(lid_lapping_Thickness + box_total_width).center(lid_Lapping_Offset,0)\
 .lineTo(Maximum_Size[0]/2, 0).spline(Bezier_Cut_Points,includeCurrent=True).lineTo(0,Maximum_Size[1]/2).close()\
 .extrude(lid_lapping_Thickness);
 
@@ -182,8 +183,7 @@ casing_cable_cut = cq.Workplane("YZ").transformed(
     rotate=cq.Vector(0, Cable_hole_angle,0));
 # Create a cylinder to represent the material to be removed by the cut.
 casing_cable_cut = casing_cable_cut.circle(Cable_hole_diam/2).extrude(-Cable_hole_Cut_Depth)
-#if DEBUG_MODE :
-debug(casing_cable_cut, name='cable-hole cut volume');
+if DEBUG_MODE : debug(casing_cable_cut, name='cable-hole cut volume');
 # Subtract the resulting geometary.
 casing_geometry = casing_geometry.cut(casing_cable_cut);
 
